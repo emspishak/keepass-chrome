79bda565012fa430ee0923e8067e3866e0ef05db
Eric Spishak <emspishak@gmail.com>
2014-02-23 19:15:49 -0800

    Decrypt with AES.
    This currently hardcodes a password and will read in a password in a later commit.

--- binary_reader.js
+++ binary_reader.js
@@ -7,6 +7,10 @@ BinaryReader.prototype.hasNextByte = function() {
   return this.pos_ < this.data_.length;
 };
 
+BinaryReader.prototype.hasNextInt = function() {
+  return this.pos_ < this.data_.length - 3;
+};
+
 BinaryReader.prototype.readByte = function() {
   if (!this.hasNextByte()) {
     throw new RangeError();
@@ -29,4 +33,32 @@ BinaryReader.prototype.readInt = function() {
     result = (result * 256) + bytes[i];
   }
   return result;
+};
+
+BinaryReader.prototype.readWord = function() {
+  var bytes = this.readBytes(4);
+  var result = 0;
+  for (var i = 0; i < bytes.length; i++) {
+    result = (result * 256) + bytes[i];
+  }
+  return result;
+};
+
+BinaryReader.prototype.readWordArray = function(num) {
+  var words = [];
+  while (num > 0) {
+    words.push(this.readWord());
+    num -= 4;
+  }
+  return CryptoJS.lib.WordArray.create(words);
+};
+
+BinaryReader.prototype.readRestToWordArray = function() {
+  var restOfFile = [];
+  var numBytes = 0;
+  while (this.hasNextInt()) {
+    restOfFile.push(this.readWord());
+    numBytes += 4;
+  }
+  return CryptoJS.lib.WordArray.create(restOfFile, numBytes);
 };
--- key_file_parser.js
+++ key_file_parser.js
@@ -8,11 +8,22 @@ KeyFileParser.DATABASE_VERSION_MASK = 4294967040;
 
 KeyFileParser.prototype.parse = function() {
   var result = {};
-  result['header'] = this.parseHeader_();
-  if (!this.verifyVersion_(result['header'])) {
+  var header = this.parseHeader_();
+  result['header'] = header;
+  if (!this.verifyVersion_(header)) {
     result['error'] = 'Invalid key file version';
     return result;
   }
+  var encryptedData = this.bytes_.readRestToWordArray();
+  var key = this.transformKey_(header['masterSeed'], header['masterSeed2'],
+      header['keyEncryptionRounds']);
+  var decryptedData = this.decryptFile_(header['flags'], encryptedData, key,
+      header['encryptionInitialValue'], header['contentsHash']);
+  if (decryptedData instanceof String) {
+    result['error'] = decryptedData;
+    return result;
+  }
+  result['decryptedData'] = decryptedData;
   return result;
 };
 
@@ -22,12 +33,12 @@ KeyFileParser.prototype.parseHeader_ = function() {
     "signature2": this.bytes_.readInt(),
     "flags": this.parseHeaderFlags_(),
     "version": this.bytes_.readInt(),
-    "masterSeed": this.bytes_.readBytes(16),
-    "encryptionInitialValue": this.bytes_.readBytes(16),
+    "masterSeed": this.bytes_.readWordArray(16),
+    "encryptionInitialValue": this.bytes_.readWordArray(16),
     "groups": this.bytes_.readInt(),
     "entries": this.bytes_.readInt(),
-    "contentsHash": this.bytes_.readBytes(32),
-    "masterSeed2": this.bytes_.readBytes(32),
+    "contentsHash": this.bytes_.readWordArray(32),
+    "masterSeed2": this.bytes_.readWordArray(32),
     "keyEncryptionRounds": this.bytes_.readInt()
   };
 };
@@ -47,4 +58,50 @@ KeyFileParser.prototype.verifyVersion_ = function(header) {
       && header['signature2'] == KeyFileParser.DATABASE_SIGNATURE_2
       && (header['version'] & KeyFileParser.DATABASE_VERSION_MASK)
           == (KeyFileParser.DATABASE_VERSION & KeyFileParser.DATABASE_VERSION_MASK);
+};
+
+KeyFileParser.prototype.transformKey_ = function(masterSeed, masterSeed2,
+      keyEncryptionRounds) {
+  var plainTextKey = 'testing';
+  var hashedKey = CryptoJS.SHA256(plainTextKey);
+  var encrypted = hashedKey;
+  var cfg = {
+    "mode": CryptoJS.mode.ECB,
+    "padding": CryptoJS.pad.NoPadding
+  };
+  for (var i = 0; i < keyEncryptionRounds; i++) {
+    encrypted = CryptoJS.AES.encrypt(encrypted, masterSeed2, cfg).ciphertext;
+  }
+  return CryptoJS.SHA256(masterSeed.concat(CryptoJS.SHA256(encrypted)));
+};
+
+KeyFileParser.prototype.decryptFile_ = function(headerFlags, encryptedData, key,
+      encryptionInitialValue, contentsHash) {
+  var cipherParams = CryptoJS.lib.CipherParams.create({
+      ciphertext: encryptedData
+  });
+  var decryptedData;
+  if (headerFlags['rijndael']) {
+    decryptedData = this.decryptAes_(cipherParams, key, encryptionInitialValue);
+  } else if (headerFlags['twofish']) {
+    throw new Exception('twofish decryption not yet supported.');
+  } else {
+    return 'Invalid encryption type';
+  }
+  var hash = CryptoJS.SHA256(decryptedData);
+  if (hash.toString() !== contentsHash.toString()) {
+    return 'Invalid password';
+  }
+  return decryptedData;
+};
+
+KeyFileParser.prototype.decryptAes_ = function(cipherParams, key, encryptionInitialValue) {
+  var cfg = {
+    "mode": CryptoJS.mode.CBC,
+    "iv": encryptionInitialValue,
+    "padding": CryptoJS.pad.Pkcs7
+  };
+  var decryptedData = CryptoJS.AES.decrypt(cipherParams, key, cfg);
+  decryptedData.clamp();
+  return decryptedData;
 };
