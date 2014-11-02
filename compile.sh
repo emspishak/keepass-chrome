c97df89f69a7f2a301d26764d24a6a2aa11284f9
Eric Spishak <emspishak@gmail.com>
2014-11-02 15:44:22 -0800

    Add a compile script.

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILES="popup.js key_file_parser.js binary_reader.js entry.js group.js key_file_header.js"
+CHROME_EXTERNS="https://raw.githubusercontent.com/google/closure-compiler/master/contrib/externs/chrome_extensions.js"
+OUTPUT="output/keepass-chrome.js"
+
+gjslint *.js
+closure-compiler \
+  --js_output_file $OUTPUT \
+  --compilation_level ADVANCED_OPTIMIZATIONS \
+  --warning_level=VERBOSE \
+  --externs externs.js <(curl $CHROME_EXTERNS) \
+  --language_in ECMASCRIPT5_STRICT \
+  $FILES
