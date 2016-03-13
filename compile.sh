4d5fc42f964c043baab2019a013fed1f44da372b
Eric Spishak <emspishak@gmail.com>
2016-03-12 17:25:55 -0800

    Remove downloaded extern now that it's included in the Closure Compiler release.

--- compile.sh
+++ compile.sh
@@ -2,8 +2,6 @@
 
 FILES="popup.js key_file_parser.js binary_reader.js entry.js group.js key_file_header.js transform_key_params.js decrypt_params.js decrypt_progress_bar.js"
 CHROME_EXTERNS="https://raw.githubusercontent.com/google/closure-compiler/master/contrib/externs/chrome_extensions.js"
-# Temporary until new Closure Compiler release.
-ENCODING_EXTERNS="https://raw.githubusercontent.com/google/closure-compiler/master/externs/w3c_encoding.js"
 OUTPUT_DIR="output"
 OUTPUT="$OUTPUT_DIR/keepass-chrome.js"
 
@@ -14,6 +12,6 @@ closure-compiler \
   --js_output_file $OUTPUT \
   --compilation_level ADVANCED_OPTIMIZATIONS \
   --warning_level=VERBOSE \
-  --externs externs.js <(curl -s $CHROME_EXTERNS) <(curl -s $ENCODING_EXTERNS) \
+  --externs externs.js <(curl -s $CHROME_EXTERNS) \
   --language_in ECMASCRIPT5_STRICT \
   --js $FILES
